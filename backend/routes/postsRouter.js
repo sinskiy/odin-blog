1b87b5e3a07afda9d267d5bd13f7cb3f97680c46
sinskiy <sinskiy@proton.me>
2024-08-25 11:50:46 +0300

    feat: use controller in postsRouter

--- backend/routes/postsRouter.js
+++ backend/routes/postsRouter.js
@@ -1,7 +1,21 @@
 const { Router } = require("express");
 const { isUser } = require("../controllers/authMiddleware");
+const {
+  postsGet,
+  postsPost,
+  postGet,
+  postPut,
+  postDelete,
+} = require("../controllers/postsController");
 const router = Router();
 
-router.get("/", isUser);
+router.get("/", postsGet);
+router.post("/", isUser, postsPost);
+
+router.get("/:postId", postGet);
+
+// TODO: replace with isAuthor
+router.put("/:postId", isUser, postPut);
+router.delete("/:postId", isUser, postDelete);
 
 module.exports = router;
