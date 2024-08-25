29d43236e4568093bec2f141dc6b9e82679e0e70
sinskiy <sinskiy@proton.me>
2024-08-25 11:43:48 +0300

    feat(backend): add (untested) posts methods

--- backend/controllers/postsController.js
+++ backend/controllers/postsController.js
@@ -1,11 +1,82 @@
-async function postsGet(req, res, next) {}
+const prisma = require("../prisma");
 
-async function postsPost(req, res, next) {}
+async function postsGet(req, res, next) {
+  try {
+    const posts = await prisma.post.findMany({
+      include: { authors: true },
+    });
+    res.json(posts);
+  } catch (err) {
+    next(err);
+  }
+}
 
-async function postGet(req, res, next) {}
+async function postsPost(req, res, next) {
+  const { authorsIds, title, description, text } = req.body;
+  try {
+    const authors = await prisma.author.findMany({
+      where: {
+        id: { in: authorsIds },
+      },
+    });
+    const post = await prisma.post.create({
+      data: {
+        title,
+        description,
+        text,
+        authors: authors,
+      },
+    });
+    res.json(post);
+  } catch (err) {
+    next(err);
+  }
+}
 
-async function postPut(req, res, next) {}
+async function postGet(req, res, next) {
+  const { id } = req.body;
+  try {
+    const post = await prisma.post.findUniqueOrThrow({
+      where: {
+        id: id,
+      },
+    });
+    res.json(post);
+  } catch (err) {
+    next(err);
+  }
+}
 
-async function postDelete(req, res, next) {}
+async function postPut(req, res, next) {
+  const { id, title, description, text } = req.body;
+  try {
+    const post = await prisma.post.update({
+      data: {
+        title,
+        description,
+        text,
+      },
+      where: {
+        id: id,
+      },
+    });
+    res.json(post);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function postDelete(req, res, next) {
+  const { id } = req.body;
+  try {
+    const post = await prisma.post.delete({
+      where: {
+        id: id,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+}
 
 module.exports = { postsGet, postsPost, postGet, postPut, postDelete };
