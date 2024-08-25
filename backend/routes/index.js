f121f46e9fba0cb8e21d9851132112219749c2e3
sinskiy <sinskiy@proton.me>
2024-08-25 14:41:20 +0300

    refactor: change structure to monorepo best practices (2)

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const authRouter = require("./authRouter");
-const postsRouter = require("./postsRouter");
-const rootRouter = Router();
-
-rootRouter.use("/auth", authRouter);
-rootRouter.use("/posts", postsRouter);
-
-module.exports = rootRouter;
