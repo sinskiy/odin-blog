b0a377671c5011911dac906948a480bae392379e
sinskiy <sinskiy@proton.me>
2024-08-27 15:08:09 +0300

    fix(client): comment typeProps

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,6 +1,14 @@
+import { shape, string } from "prop-types";
+
 const Comment = ({ comment }) => {
   console.log(comment);
   return <div>{comment.text}</div>;
+};
+Comment.propTypes = {
+  comment: shape({
+    text: string.isRequired,
+    created_at: string.isRequired,
+  }),
 };
 
 export default Comment;
